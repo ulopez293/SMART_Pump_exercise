9ca7ce98584540a08098c47f82519bf7e8ea3b93
Ubaldo Lopez <ulopez293@gmail.com>
2023-05-16 23:25:38 -0600

    remove unnecessary code

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,10 +1,6 @@
 import { join } from 'path'
 import fs from 'fs'
 
-interface Low {
-    data: Array<string>
-}
-
 export const getConnection =  async () => {
     try {
         const { Low } = await import('lowdb')
@@ -13,7 +9,7 @@ export const getConnection =  async () => {
         const data = fs.readFileSync(file, 'utf-8')
         const adapter = new JSONFile(file)
         const defaultData = JSON.parse(data)
-        const db: Low = new Low(adapter, defaultData)
+        const db = new Low(adapter, defaultData)
         return db
     } catch (error) {
         console.error(`Error to read file JSON: ${error}`)
